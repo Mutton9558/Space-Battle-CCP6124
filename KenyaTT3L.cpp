811a9d8553b7d674907c0610e84f40198c8ca55a
zixuan1019 <zixuansoh1019@gmail.com>
2026-01-19 15:16:20 +0800

    battle function & output

--- KenyaTT3L.cpp
+++ KenyaTT3L.cpp
@@ -13,6 +13,7 @@
 #include <sstream>
 #include <ctime>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -90,28 +91,29 @@ protected:
     map<string, int> maxCrewType;
     string shipName;
     vector<Crew *> CrewMembers;
-    float lightCannotHitChance, torpedoHitChance;
+    float lightHitChance, torpedoHitChance;
 
 public:
-    Ship(int hitpoint, int id, string name, int maxp, int maxg, int maxth, float LCHitChance, float THitChance)
+    Ship(int hitpoint, int id, string name, int maxP, int maxG, int maxT, float LCH, float TH)
+        : hp(hitpoint), shipID(id), shipName(name),
+          lightHitChance(LCH), torpedoHitChance(TH)
     {
-        hp = hitpoint;
-        shipID = id;
-        shipName = name;
-        maxCrewType["Pilot"] = maxp;
-        maxCrewType["Gunner"] = maxg;
-        maxCrewType["Torpedo Handler"] = maxth;
-        lightCannotHitChance = LCHitChance;
-        torpedoHitChance = THitChance;
+        maxCrewType["Pilot"] = maxP;
+        maxCrewType["Gunner"] = maxG;
+        maxCrewType["Torpedo Handler"] = maxT;
+        crewType["Pilot"] = 0;
+        crewType["Gunner"] = 0;
+        crewType["Torpedo Handler"] = 0;
     }
 
     virtual int returnLightCannonDamage() const = 0;
-
-    virtual int returnTorpedoDamage() const { return 0; };
+    virtual int returnTorpedoDamage() const { return 0; }
+    float getLightHitChance() const { return lightHitChance * 100; }
+    float getTorpedoHitChance() const { return torpedoHitChance * 100; }
 
     void isHit(string attackType, int damage)
     {
-        int hitChance = (((maxCrewType["Pilot"] - crewType["Pilot"]) * 0.25 + 1) * (attackType == "Light Cannon" ? lightCannotHitChance : torpedoHitChance)) * 1000;
+        int hitChance = (((maxCrewType["Pilot"] - crewType["Pilot"]) * 0.25 + 1) * (attackType == "Light Cannon" ? lightHitChance : torpedoHitChance)) * 1000;
         // for example if hit == 490 and hit chance was 550, it hits.
         // easier to illustrate with graph but essentially
         // 0 ------  50 ------ 100
@@ -142,10 +144,9 @@ public:
         CrewMembers.push_back(c);
     }
 
-    int returnHP() const
-    {
-        return hp;
-    }
+    int returnHP() const { return hp; }
+    string returnName() const { return shipName; }
+    const vector<Crew *> &getCrew() const { return CrewMembers; }
 
     int returnMaxPilot() const
     {
@@ -426,6 +427,10 @@ void shipAssignment(const char *crew, const char *ship, const string team)
             }
         }
 
+        sort(teamShips->begin(), teamShips->end(),
+             [](Ship *a, Ship *b)
+             { return a->returnHP() > b->returnHP(); });
+
         // Assign crews to ships (round-robin)
         size_t shipIndex = 0;
         while (!crewQueue.empty() && !teamShips->empty())
@@ -433,17 +438,27 @@ void shipAssignment(const char *crew, const char *ship, const string team)
             Crew *c = crewQueue.front();
             crewQueue.pop();
 
-            Ship *s = (*teamShips)[shipIndex % teamShips->size()];
-            if (s->validAssignment(c))
+            size_t attempts = 0;
+            bool assigned = false;
+
+            while (attempts < teamShips->size())
             {
-                s->insertCrew(c);
-            }
-            else
-            {
-                delete c; // Discard if cannot assign
+                Ship *s = (*teamShips)[shipIndex % teamShips->size()];
+                if (s->validAssignment(c))
+                {
+                    s->insertCrew(c);
+                    assigned = true;
+                    shipIndex++; // next crew goes to next ship
+                    break;
+                }
+                shipIndex++;
+                attempts++;
             }
 
-            shipIndex++;
+            if (!assigned)
+            {
+                delete c; // no ship can take this crew
+            }
         }
     }
     catch (...)
@@ -452,24 +467,150 @@ void shipAssignment(const char *crew, const char *ship, const string team)
     }
 }
 
+// battle function
+bool attackHits(float chancePercent)
+{
+    return (rand() % 100) < chancePercent;
+}
+
+Ship *pickTarget(vector<Ship *> &fleet)
+{
+    vector<Ship *> alive;
+    for (auto s : fleet)
+        if (s->returnHP() > 0)
+            alive.push_back(s);
+    if (alive.empty())
+        return nullptr;
+    return alive[rand() % alive.size()];
+}
+
+void simulateRound(vector<Ship *> &fleetA, vector<Ship *> &fleetB)
+{
+    auto fleetAttack = [](vector<Ship *> &attacker, vector<Ship *> &defender)
+    {
+        for (auto s : attacker)
+        {
+            if (s->returnHP() <= 0)
+                continue;
+            for (auto c : s->getCrew())
+            {
+                Ship *target = pickTarget(defender);
+                if (!target)
+                    continue;
+                int dmg = 0;
+                float chance = 0;
+                string weapon;
+                if (c->returnType() == "Gunner")
+                {
+                    dmg = s->returnLightCannonDamage();
+                    chance = s->getLightHitChance();
+                    weapon = "Light Cannon";
+                }
+                else if (c->returnType() == "Torpedo Handler")
+                {
+                    dmg = s->returnTorpedoDamage();
+                    chance = s->getTorpedoHitChance();
+                    weapon = "Torpedo";
+                }
+                else
+                    continue;
+                bool hit = attackHits(chance);
+                if (hit)
+                    target->isHit(weapon, dmg);
+                cout << c->returnType() << " on " << s->returnName() << " fires " << weapon
+                     << " at " << target->returnName() << "... " << (hit ? "HIT!" : "MISS!");
+                if (hit)
+                    cout << " Remaining HP: " << target->returnHP() << (target->returnHP() <= 0 ? " (DESTROYED)" : "");
+                cout << endl;
+            }
+        }
+    };
+
+    fleetAttack(fleetA, fleetB);
+    fleetAttack(fleetB, fleetA);
+}
+
+bool fleetDestroyed(vector<Ship *> &fleet)
+{
+    for (auto s : fleet)
+        if (s->returnHP() > 0)
+            return false;
+    return true;
+}
+
+void simulateBattle(vector<Ship *> &fleetA, vector<Ship *> &fleetB, const string &nameA, const string &nameB)
+{
+    int round = 1;
+    while (!fleetDestroyed(fleetA) && !fleetDestroyed(fleetB))
+    {
+        cout << "\n=== ROUND " << round << " ===\n";
+        simulateRound(fleetA, fleetB);
+        round++;
+    }
+    cout << "\n=== FINAL RESULT ===\n";
+    if (fleetDestroyed(fleetA))
+        cout << "*** " << nameB << " WIN! ***\n";
+    else
+        cout << "*** " << nameA << " WIN! ***\n";
+
+    cout << "\nSurviving ships:\n";
+    for (auto s : fleetA)
+        if (s->returnHP() > 0)
+            cout << nameA << " - " << s->returnName() << ": " << s->returnHP() << " HP\n";
+    for (auto s : fleetB)
+        if (s->returnHP() > 0)
+            cout << nameB << " - " << s->returnName() << ": " << s->returnHP() << " HP\n";
+}
+
 // argc is argument count, argv is the argument variables
 int main(const int argc, const char *argv[])
 {
     srand(time(0));
-    // argc also includes calling the process
+
     if (argc != 5)
     {
         cout << "Please enter the ship.csv and crew.csv for Zapezoid and Rogoatuskan!" << endl;
+        return 1;
     }
 
+    // Assign crews to ships for both teams
     int count = 1;
     while (count < argc)
     {
-        // ill change this later fr lazy rn
         string team = (argv[count][0] == 'r' ? "Rogoatuskan" : "Zapezoid");
-        // XyloTT9L zShips1.csv zCrew1.csv rShips1.csv rCrew1.csv <- format of call
         shipAssignment(argv[count + 1], argv[count], team);
-        count = count + 2;
+        count += 2;
     }
-    cout << "Hello World" << endl;
+
+    cout << "Hello World\n";
+    cout << "=== CREW ASSIGNMENT ===\n\n";
+
+    // Lambda to print a team
+    auto printTeam = [](const string &teamName, const vector<Ship *> &ships)
+    {
+        cout << "Team: " << teamName << endl;
+        for (Ship *s : ships)
+        {
+            cout << "Ship: " << s->returnHP() << " HP, Name: " << s->returnName() << endl;
+            cout << "Crew: ";
+            const auto &crewList = s->getCrew();
+            for (size_t i = 0; i < crewList.size(); ++i)
+            {
+                cout << *crewList[i];
+                if (i != crewList.size() - 1)
+                    cout << "; ";
+            }
+            cout << "\n\n";
+        }
+    };
+
+    // Print both teams' crew assignments
+    printTeam("Rogoatuskan", RogoatuskanShip);
+    printTeam("Zapezoid", ZapezoidShip);
+
+    // Start the battle
+    cout << "=== BATTLE START ===\n\n";
+    simulateBattle(RogoatuskanShip, ZapezoidShip, "Rogoatuskan", "Zapezoid");
+
+    return 0;
 }
